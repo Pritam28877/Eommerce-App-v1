45e53914919e9433371b3b87ef29c6f99dc374db
Pritam Das <dpritam28577@gmail.com>
2023-09-17 21:10:28 +0530

    add the Toaster Provider

--- components/providers.tsx
+++ components/providers.tsx
@@ -11,5 +11,17 @@ interface Props {
 }
 
 export function Providers({ children }: Props) {
-  return <>{children}</>
+  return (
+    <CartProvider
+      currency="USD"
+      shouldPersist={true}
+      cartMode="checkout-session"
+      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
+    >
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Toaster />
+        {children}
+      </ThemeProvider>
+    </CartProvider>
+  )
 }
