8334a965a007c16510dd2d85e465cf30cc5e6a29
Pritam Das <dpritam28577@gmail.com>
2023-09-19 20:21:36 +0530

    add the schema for the product

--- sanity/schemas/product-schema.ts
+++ sanity/schemas/product-schema.ts
@@ -1,1 +1,66 @@
-export const product = {}
+import { defineField, defineType } from "sanity"
+
+export const product = defineType({
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+      },
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      of: [{ type: "image" }],
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "size",
+      title: "Size",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "color",
+      title: "Color",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "sku",
+      title: "Sku",
+      type: "string",
+    },
+    {
+      name: "currency",
+      title: "Currency",
+      type: "string",
+    },
+    {
+      name: "price",
+      title: "Price",
+      type: "number",
+    },
+  ],
+})
