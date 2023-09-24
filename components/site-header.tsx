17952f112a7afda54797b7dddb78dd7cdba6fe20
Pritam Das <dpritam28577@gmail.com>
2023-09-24 20:33:02 +0530

    add the search queary through input

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,15 +12,25 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   const pathname = usePathname()
+  const router = useRouter()
 
   if (pathname.startsWith("/studio")) {
     return null
+  }
+  function onsubmit (e: React.SyntheticEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const search = formData.get("search")
+    router.replace(`/?search=${search}`)
   }
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
+        <form
+          onSubmit={onsubmit}
+          className="hidden items-center lg:inline-flex"
+        >
           <Input
             id="search"
             name="search"
@@ -32,9 +42,7 @@ export function SiteHeader() {
         </form>
         <div className="flex items-center space-x-1">
           <Link href="/cart">
-            <Button
-              style={{ fontSize: "small", backgroundColor: "#fff" }}
-            >
+            <Button style={{ fontSize: "small", backgroundColor: "#fff" }}>
               <ShoppingBag className="h-5 w-5" />
               <span className="ml-2 text-sm font-bold">0</span>
               <span className="sr-only">Cart</span>
@@ -43,9 +51,7 @@ export function SiteHeader() {
           <ThemeToggle />
           {process.env.NODE_ENV === "development" && (
             <Link href="/studio">
-              <Button
-                style={{ fontSize: "small", backgroundColor: "#fff" }}
-              >
+              <Button style={{ fontSize: "small", backgroundColor: "#fff" }}>
                 <Edit className="h-5 w-5" />
                 <span className="sr-only">Admin</span>
               </Button>
